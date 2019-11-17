064f01b81d1304f88c7e47010951715861d80408
Pat Brown <ptrckbrown94@gmail.com>
2019-11-17 16:59:02 -0600

    deleted extra html pages and got button to work

---
+++ questions.js
@@ -0,0 +1,23 @@
+//global varibles
+let questionIndex = 0;
+
+
+
+//var questions = [
+//    {
+//      title: "Commonly used data types DO NOT include:",
+//      choices: ["strings", "booleans", "alerts", "numbers"],
+//      answer: "alerts"
+//    },
+//    {
+//      title: "The condition in an if / else statement is enclosed within ____.",
+//      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+//      answer: "parentheses"
+//    },
+
+function StartQuiz(){
+    console.log("test");
+    // https://www.w3schools.com/howto/howto_js_redirect_webpage.asp
+    window.location.href = "questionPage.html";
+    questionIndex = 0;
+}
