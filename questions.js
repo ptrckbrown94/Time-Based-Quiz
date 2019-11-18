e5db603a11703991141fd94895139e5fd5ded5ae
Pat Brown <ptrckbrown94@gmail.com>
2019-11-18 09:41:34 -0600

    delete extra html

--- questions.js
+++ questions.js
@@ -23,6 +23,15 @@ var questions = [
         answer: "parentheses"
     }];
 
+
+// when I navigate to a new page with 
+//    window.location.href = "questionPage.html";
+// or
+//    location.assign("questionPage.html"); 
+// The js file seems to always "reload" so the myFunctionWasCalled is false on the new page.
+// If I call doFoo() on the new page, if it was called before I would expect the value to then be true!
+// So, if the variable always resets, how will I keep track of high scores?
+
 var myFunctionWasCalled = false;
 
 function doFoo()
