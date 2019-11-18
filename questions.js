5d72bf10cd33add1a7f7cff2a0d5042772fe7c4b
Pat Brown <ptrckbrown94@gmail.com>
2019-11-18 15:17:23 -0600

    fixing html and js

--- questions.js
+++ questions.js
@@ -24,13 +24,6 @@ var questions = [
     }];
 
 
-// when I navigate to a new page with 
-//    window.location.href = "questionPage.html";
-// or
-//    location.assign("questionPage.html"); 
-// The js file seems to always "reload" so the myFunctionWasCalled is false on the new page.
-// If I call doFoo() on the new page, if it was called before I would expect the value to then be true!
-// So, if the variable always resets, how will I keep track of high scores?
 
 var myFunctionWasCalled = false;
 
@@ -38,10 +31,8 @@ var myFunctionWasCalled = false;
 
 function StartQuiz() {
     console.log("test");
-    // https://www.w3schools.com/howto/howto_js_redirect_webpage.asp
-    
-    //window.location.href = "questionPage.html";
-    location.assign("questionPage.html");
+
+
 
     questionIndex = 0;
 
@@ -51,19 +42,6 @@ function StartQuiz() {
 
 }
 
-function ClickA() {
-
-}
-
-function ClickB() {
-
-}
-
-function ClickC() {
-
-}
-
-function ClickD() {
 
 }
 
