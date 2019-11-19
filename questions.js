3ef800ae6d148981183fd4bdb518007d4186d123
Pat Brown <ptrckbrown94@gmail.com>
2019-11-18 21:18:14 -0600

    trying to delete container

--- questions.js
+++ questions.js
@@ -1,7 +1,10 @@
 //global varibles
 let questionIndex = 0;
+const mainContent = document.getElementById("mainContent")
+function qustion(){}
 
-var questions = [
+
+let questions = [
     {
         title: "Which is the best Star Wars video Game:",
         choices: ["Star Wars: Shadows of the Empire", "Super Empire Strikes Back", "Lego Star Wars the complete Saga", "Star Wars: Knights of the Old Replic"],
@@ -21,7 +24,7 @@ var questions = [
         title: "Who is the most power full Jedi:",
         choices: ["Kit Fisto", "Yoda", "Luke Skywalker", "Qui-Gon Jinn"],
         answer: "Luke Skywalker"
-    }
+    },
 
     {
         title: "Worst Character in the Star Wars movie Universe:",
@@ -30,25 +33,25 @@ var questions = [
     }];
 
 
-var myFunctionWasCalled = false;
-
-
 
 function StartQuiz() {
     console.log("test");
-
-
-
+    
     questionIndex = 0;
 
+    //LoadQuestion(questionIndex)
 
-    LoadQuestion(questionIndex)
+    function clearBox(mainContent)
+    {
+        document.getElementById(elementID).innerHTML = "";
+    }
+    
 
 
 }
 
 
-}
+
 
 function LoadQuestion(index) {
 
@@ -56,10 +59,18 @@ function LoadQuestion(index) {
 
     let title = question.title;
 
-    console.log("The question title is: " + title)
+    console.log("The question title is: " + title);
 
     let h2TextElement = document.getElementById("questionText");
 
     h2TextElement.nodeValue = title;
 
 }
+
+
+//bootstrap cards
+// {/* <div class="card">
+// <div class="card-body">
+// This is some text within a card body.
+// </div>
+// </div> */}
